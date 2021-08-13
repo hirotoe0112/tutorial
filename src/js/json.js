a59bfcbfcc28d63f6507d02b5d41e6e39d38278a
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2021-08-13 11:28:09 +0900

    パスの変更

--- src/js/json.js
+++ src/js/json.js
@@ -4,13 +4,14 @@ class DealJsonClass{
 
   //全データ読み込み
   readalldata(){
-    let data = JSON.parse(fs.readFileSync('data.json'));
+    console.log(__dirname);
+    let data = JSON.parse(fs.readFileSync('./src/data.json'));
     return data;
   }
 
   //ファイル名を指定して内容を読み込み
   readfilecontent(filename){
-    let data = JSON.parse(fs.readFileSync('data.json'));
+    let data = JSON.parse(fs.readFileSync('./src/data.json'));
 
   }
 
