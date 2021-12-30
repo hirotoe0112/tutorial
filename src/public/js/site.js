fb59c8b2cbb03f9031e7e80efc6f732f652bc457
hirotoe0112 <hirotoe0112@gmail.com>
2021-12-31 08:07:15 +0900

    ルーティング修正

--- src/public/js/site.js
+++ src/public/js/site.js
@@ -1,6 +1,5 @@
 //ページ読み込み時処理
 document.addEventListener('DOMContentLoaded', function(){
-
   //各記事に対するイベント登録
   var items = document.querySelectorAll('.item');
   for(let i = 0; i < items.length; i++){
@@ -14,9 +13,20 @@ document.addEventListener('DOMContentLoaded', function(){
 }, false);
 
 //モーダルウインドウ表示
-OpenDetail = function(){
+OpenDetail = async function(){
+  //記事内容取得
+  await GetContent();
+
+  //モーダル表示
   console.log('open');
   document.querySelector('#detail').classList.add('open');
+}
+
+//記事内容取得
+GetContent = async function(){
+  const res = await fetch('/test');
+  const text = await res.text();
+  console.log(text);
 }
 
 //モーダルウインドウ非表示
