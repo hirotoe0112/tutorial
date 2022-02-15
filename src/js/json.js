dc7fe6c1d3684a791f52c459dfbea3b1f9916137
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2022-02-15 18:39:34 +0900

    ファイル内容取得

--- src/js/json.js
+++ src/js/json.js
@@ -1,18 +1,21 @@
 const fs = require('fs');
+const join = require('path').join;
+var datafile = './src/data.json';
+var data = JSON.parse(fs.readFileSync(datafile));
 
 class DealJsonClass{
 
   //全データ読み込み
   readalldata(){
-    console.log(__dirname);
-    let data = JSON.parse(fs.readFileSync('./src/data.json'));
     return data;
   }
 
   //ファイル名を指定して内容を読み込み
   readfilecontent(filename){
-    let data = JSON.parse(fs.readFileSync('./src/data.json'));
-
+    console.log(filename);
+    var datapath = data.path;
+    var content = fs.readFileSync(join(datapath, filename));
+    return content.toString();
   }
 
 }
